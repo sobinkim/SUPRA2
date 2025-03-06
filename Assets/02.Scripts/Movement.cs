5040dff3c86be9df8d00dbcf6a14b234cade0d06
sobinkim08 <sobinkim95@gmail.com>
2025-03-06 23:46:51 +0900

    add Map, movement

--- Assets/02.Scripts/Movement.cs
+++ Assets/02.Scripts/Movement.cs
@@ -1,30 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
-
 public class Movement : MonoBehaviour
 {
     private Vector2 direction;
     [SerializeField] Rigidbody rigid;
-    private float _mass;
-    private float _maxSD;
 
-    private float _velocity;
+    [SerializeField] private float mass;
+    [SerializeField] private float gravity;
+    [SerializeField] private float max_km_Speed;
+    [SerializeField] private float max_Time;
+    [SerializeField] private float checkSpeed;
+    private float currentSpeed = 0;
+    private float power;
+    private float max_ms_Speed;
+
+    private void Start()
+    {
+        max_ms_Speed = max_km_Speed * (5f / 18f);
+        print(max_ms_Speed);
+    }
     public void SetDir(Vector2 dir)
     {
+     
         direction = dir;
+        print(dir);
     }
-    public void Calculator()
+
+    private void GetSpeed()
     {
+        Vector2 vector = direction;
+        if (vector.magnitude != 0)
+        {
+            DOTween.To(() => currentSpeed, x => currentSpeed = x, max_ms_Speed, max_Time)
+              .SetEase(Ease.InCubic);
+         
+        
+        }
+        else if (vector.magnitude == 0)
+        {
+            DOTween.To(() => currentSpeed, x => currentSpeed = x, 0, 2)
+           .SetEase(Ease.OutQuad);
+         
+
+        }
+        else if (rigid.velocity.magnitude == 0)
+        {
+            rigid.velocity = Vector3.zero;
+         
+        }
+
 
     }
+
+    public void Calculator_Move()
+    {
+        power = mass * currentSpeed;
+        rigid.AddForce(direction * power, ForceMode.Force);
+
+    }
+
     private void Update()
     {
-        Move();
+       
+        GetSpeed();
+        checkSpeed = currentSpeed;
     }
 
-    private void Move()
+    private void FixedUpdate()
     {
-        rigid.AddForce(new Vector3(direction.x, direction.y, 0));
+        Calculator_Move();
     }
+
+
 }
